72ebcfb0fcc1196a2471723892816be5a097c5a4
Joannis orlandos <joannis@orlandos.nl>
2021-06-22 22:46:01 +0200

    Fix throwing an error on success

--- Sources/Citadel/SFTP/SFTPFile.swift
+++ Sources/Citadel/SFTP/SFTPFile.swift
@@ -8,7 +8,7 @@ public struct SFTPFile {
         client.readFile(handle: handle, offset: offset, length: length)
     }
     
-    public func write(from offset: UInt64 = 0, length: UInt32, data: ByteBuffer) -> EventLoopFuture<Void> {
-        client.writeFile(handle: handle, data: data, offset: offset, length: length)
+    public func write(at offset: UInt64 = 0, data: ByteBuffer) -> EventLoopFuture<Void> {
+        client.writeFile(handle: handle, data: data, offset: offset, length: UInt32(data.readableBytes))
     }
 }
--- Tests/CitadelTests/Citadel2Tests.swift
+++ Tests/CitadelTests/Citadel2Tests.swift
@@ -22,8 +22,20 @@ final class Citadel2Tests: XCTestCase {
         XCTAssertEqual(buffer.readableBytes, 0)
         XCTAssertEqual(bigInt, sameBigInt)
     }
+  
+    func testSFTP() throws {
+        let ssh = try SSHClient.connect(
+          host: "orlandos.nl",
+          authenticationMethod: .passwordBased(username: "<user>", password: "<pass>"),
+          hostKeyValidator: .acceptAnything(), // It's easy, but you should put your hostkey signature in here
+          reconnect: .never
+        ).wait()
+        let sftp = try ssh.openSFTP().wait()
+        let fileHandle = try sftp.openFile(filePath: "/home/<user>/test", flags: [.write, .create]).wait()
+        try fileHandle.write(at: 0, data: ByteBuffer(string: "Hello")).wait()
+    }
 
-    static var allTests = [
-        ("testBigIntSerialization", testBigIntSerialization),
-    ]
+//    static var allTests = [
+//        ("testBigIntSerialization", testBigIntSerialization),
+//    ]
 }
